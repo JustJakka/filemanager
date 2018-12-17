ce7339a8b2ace83df23ab16d839d73fc0fdb8a63
Smart developer <developer@yandex.ru>
2018-12-17 10:51:39 +0300

    updated

--- file_manager.py
+++ file_manager.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QFileSystemModel, QTreeView, QWidget, QVBoxLayout
 from PyQt5.QtGui import QIcon
+from tkinter import *
+from tkinter import filedialog as fd
 
 
 class Manager(QWidget):
@@ -14,6 +16,7 @@ class Manager(QWidget):
 
         self.shown = QFileSystemModel()
         self.shown.setRootPath('')
+
         self.tree = QTreeView()
         self.tree.setModel(self.shown)
 
@@ -23,14 +26,36 @@ class Manager(QWidget):
         self.tree.setWindowTitle('Directory View')
         self.tree.resize(1000, 600)
 
-        windowlayout = QVBoxLayout()
-        windowlayout.addWidget(self.tree)
-        self.setLayout(windowlayout)
+        window_layout = QVBoxLayout()
+        window_layout.addWidget(self.tree)
+        self.setLayout(window_layout)
 
-        self.tree.doubleClicked(self.type)
+    def text_editor_eval(self):
+        def insert_text():
+            file_name = fd.askopenfilename()
+            with open(file_name) as file:
+                txt = file.read()
+                text.insert(1.0, txt)
 
-    def type(self):
+        def extract_text():
+            file_name = fd.asksaveasfilename(filetypes=(("TXT files", "*.txt"),
+                                                        ("HTML files", "*.html;*.htm"),
+                                                        ("All files", "*.*")))
+            with open(file_name, mode='w') as file:
+                txt = text.get(1.0, END)
+                file.write(txt)
 
+
+        root = Tk()
+        text = Text(width=50, height=25)
+        text.grid(columnspan=2)
+        b1 = Button(text='Open', command=insert_text)
+        b1.grid(row=1, sticky=E)
+        b2 = Button(text='Save', command=extract_text)
+        b2.grid(row=1, column=1, sticky=W)
+        root.title = 'Window'
+
+        root.mainloop()
 
 
 if __name__ == '__main__':
--- text_editor.py
+++ text_editor.py
@@ -1,31 +1,30 @@
 from tkinter import *
 from tkinter import filedialog as fd
+import tkinter
 
-
-def insertText():
+def insert_text():
     file_name = fd.askopenfilename()
-    f = open(file_name)
-    s = f.read()
-    text.insert(1.0, s)
-    f.close()
+    with open(file_name) as file:
+        txt = file.read()
+        text.insert(1.0, txt)
 
 
-def extractText():
+def extract_text():
     file_name = fd.asksaveasfilename(filetypes=(("TXT files", "*.txt"),
                                                 ("HTML files", "*.html;*.htm"),
                                                 ("All files", "*.*")))
-    f = open(file_name, 'w')
-    s = text.get(1.0, END)
-    f.write(s)
-    f.close()
+    with open(file_name, mode='w') as file:
+        txt = text.get(1.0, END)
+        file.write(txt)
 
 
-root = Tk()
+root = tkinter.Tk()
+root.title = 'Window'
 text = Text(width=50, height=25)
 text.grid(columnspan=2)
-b1 = Button(text="Открыть", command=insertText)
+b1 = Button(text='Open', command=insert_text)
 b1.grid(row=1, sticky=E)
-b2 = Button(text="Сохранить", command=extractText)
+b2 = Button(text='Save', command=extract_text)
 b2.grid(row=1, column=1, sticky=W)
 
 root.mainloop()
